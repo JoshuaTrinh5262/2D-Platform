a1d7e151f42dbba9cae01fb2933263c832842a90
Trinh Quang Truong <rokudo5262@gmail.com>
2020-06-19 22:29:14 +0700

    camera follow player

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private Transform playerTransform;
+    public float offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+ 
+    }
+    void LateUpdate()
+    {
+        Vector3 temp = transform.position;
+        temp.x = playerTransform.position.x;
+        temp.y = playerTransform.position.y;
+        temp.x += offset;
+        transform.position = temp;
+    }
+}
